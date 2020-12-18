9a269242c2b6ded3e7786c47754cbb163efa462c
ojasnakade <72932558+ojasnakade@users.noreply.github.com>
2020-12-18 15:53:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c36-project
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,15 @@
+var gameState=0;
+var playerCount,form,player,game;
+var database;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+  
+}
